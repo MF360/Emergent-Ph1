37e679b73ab09be17f97b2389c7c1f74e20a9ccb
Santhosh VS <santhosh02vs@gmail.com>
2025-10-12 03:21:15 +0530

    Added responsive Pricing section with tiered plans and CTA buttons

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import Features from "../components/Features";
 import Testimonials from "../components/Testimonials";
+import Pricing from "../components/Pricing";
 
 const Index = () => {
   return (
@@ -11,6 +12,7 @@ const Index = () => {
         <HeroSection />
         <Features />
         <Testimonials />
+        <Pricing />
       </main>
     </div>
   );
