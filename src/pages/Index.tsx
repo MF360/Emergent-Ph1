d7872e9300028b857dbc7114478fc9bb63ffe92d
Santhosh VS <santhosh02vs@gmail.com>
2025-10-12 20:01:38 +0530

    fix the import

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import Features from "../components/Features";
-import Testimonials from "../components/Testimonials";
 import Pricing from "../components/Pricing";
 import Footer from "../components/Footer";
 // import DashboardPreview from "../components/DashboardPreview";
